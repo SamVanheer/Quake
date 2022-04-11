cc87d24802864f5c65c02d440f9cd37571e28f36
Sam V <sam.vanheer@outlook.com>
2022-04-11 18:59:41 +0200

    Rename gl_vidnt.cpp to vid.cpp, use for all platforms

--- WinQuake/src/winquake/vid.cpp
+++ WinQuake/src/winquake/vid.cpp
@@ -17,7 +17,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
-// gl_vidnt.c -- NT GL vid component
+// vid.cpp -- GL vid component
 
 #include <thread>
 
