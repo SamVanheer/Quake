4072f9585883b211eeee94724a7881d0bc121622
Sam V <sam.vanheer@outlook.com>
2022-04-11 19:48:18 +0200

    Remove unused header includes from sys.cpp

--- WinQuake/src/winquake/sys.cpp
+++ WinQuake/src/winquake/sys.cpp
@@ -23,8 +23,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "quakedef.h"
 #include "winquake.h"
-#include "errno.h"
-#include "resource.h"
 #include "conproc.h"
 #include <VersionHelpers.h>
 
