a3b360e6b546a5a5e4baaa70bf9078e4a0d791db
Sam V <sam.vanheer@outlook.com>
2022-07-05 21:51:02 +0200

    Use vcpkg to acquire third party dependencies

---
+++ vcpkg_config/triplets/x86-common.cmake
@@ -0,0 +1,16 @@
+# Included by platform-specific triplets to configure third party dependencies.
+# Do not use as a triplet directly.
+
+# Default to static linkage for all dependencies.
+set(VCPKG_LIBRARY_LINKAGE static)
+
+# Override linkage to dynamic only for these.
+if(PORT MATCHES "(sdl2|openal-soft)")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
+
+set(VCPKG_C_FLAGS "")
+set(VCPKG_CXX_FLAGS "-std=c++20")
+
+# We don't need debug builds.
+set(VCPKG_BUILD_TYPE "release")
